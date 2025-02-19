342633c576c7e2a46596d9ed244424990579eac9
nathan yakob <nathanwaghchoure@gmail.com>
2025-02-19 17:44:11 +0530

    # ai-finance-tracking its an ai create an ai based investment advisory  platform that provides users with personalized financial recommendations based on their risk profile , investment goals, and market trends.

---
+++ ai-finance-tracking-main/app/page.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import Header from "./_components/Header.jsx";
+import Hero from "./_components/Hero.jsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+    </div>
+  );
+}
